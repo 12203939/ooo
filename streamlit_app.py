111c8ec231c96c3e56675b427919e4d59650a4e0
12203939 <153032196+12203939@users.noreply.github.com>
2023-12-12 06:55:32 +0000

    aa

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,31 +1,16 @@
-# Streamlitライブラリをインポート
 import streamlit as st
+from scipy import stats
 
-# ページ設定（タブに表示されるタイトル、表示幅）
-st.set_page_config(page_title="タイトル", layout="wide")
+# アプリケーションのタイトルを設定
+st.title('正規分布の確率計算アプリ')
 
-# タイトルを設定
-st.title('Streamlitのサンプルアプリ')
+# ユーザーからの入力を受け取る
+mean = st.number_input('平均', value=0.0)
+std = st.number_input('標準偏差', value=1.0)
+x = st.number_input('確率変数の値', value=0.0)
 
-# テキスト入力ボックスを作成し、ユーザーからの入力を受け取る
-user_input = st.text_input('あなたの名前を入力してください')
+# 正規分布の確率を計算
+probability = stats.norm.cdf(x, loc=mean, scale=std)
 
-# ボタンを作成し、クリックされたらメッセージを表示
-if st.button('挨拶する'):
-    if user_input:  # 名前が入力されているかチェック
-        st.success(f'🌟 こんにちは、{user_input}さん! 🌟')  # メッセージをハイライト
-    else:
-        st.error('名前を入力してください。')  # エラーメッセージを表示
-
-# スライダーを作成し、値を選択
-number = st.slider('好きな数字（10進数）を選んでください', 0, 100)
-
-# 補足メッセージ
-st.caption("十字キー（左右）でも調整できます。")
-
-# 選択した数字を表示
-st.write(f'あなたが選んだ数字は「{number}」です。')
-
-# 選択した数値を2進数に変換
-binary_representation = bin(number)[2:]  # 'bin'関数で2進数に変換し、先頭の'0b'を取り除く
-st.info(f'🔢 10進数の「{number}」を2進数で表現すると「{binary_representation}」になります。 🔢')  # 2進数の表示をハイライト
+# 結果を表示
+st.write('平均が {} で標準偏差が {} の正規分布において、確率変数が {} 以下である確率は {:.4f} です。'.format(mean, std, x, probability))
